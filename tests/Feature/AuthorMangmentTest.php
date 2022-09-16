844c126b67795eaa641a56fb0452cad1f9b9c5ee
jalalmx <jalalnx99@gmail.com>
2022-09-17 01:44:09 +0200

    add author resource and test

---
+++ tests/Feature/AuthorMangmentTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Author;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthorMangmentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_Author_can_be_created()
+    {
+
+        $this->withoutExceptionHandling();
+        $this->post('/author', [
+            'name' => 'Author name ',
+            'dob' => '2014-04-02',
+        ]);
+
+        $author= Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('2014-04-02',$author->first()->dob->format('Y-m-d'));
+    }
+}
